a2fea7ebb96c4fcb7d7ab90170d86e778f8bfc39
Guilherme de Paula <78801502+Dipaula2@users.noreply.github.com>
2024-12-04 12:30:59 -0300

    Add files via upload
    ATT-04/12

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// build.gradle.kts (Projeto)
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+buildscript {
+    dependencies {
+        classpath(libs.google.services.v440)
+        classpath(libs.google.services)
+
+    }
+}
