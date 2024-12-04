0bc2edc74caea3c530690b9450b9fae771ff2e15
Guilherme de Paula <78801502+Dipaula2@users.noreply.github.com>
2024-12-04 12:32:49 -0300

    ATT-04/12
    ATT-04/12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,6 @@ android {
     defaultConfig {
         applicationId = "com.example.cyber"
         minSdk = 24
-        //noinspection OldTargetApi
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -43,7 +42,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"  // Certifique-se de que está usando a versão correta
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
     packaging {
         resources {
@@ -54,38 +53,24 @@ android {
 
 dependencies {
     // Dependências do Compose
-    implementation ("androidx.compose.material:material-icons-extended:1.7.5")
+    implementation("androidx.compose.material:material-icons-extended:1.7.5")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.ui:ui:1.5.0")
     implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
     implementation("androidx.compose.material3:material3:1.1.0")
-    implementation("androidx.navigation:navigation-compose:2.6.0")  // Para navegação com Compose
+    implementation("androidx.navigation:navigation-compose:2.6.0")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("androidx.compose.animation:animation:1.4.3")
+    implementation("io.coil-kt:coil-compose:2.2.2")
 
-    implementation (platform(libs.firebase.bom.v3231))
-    implementation(libs.firebase.ui.auth)
-    implementation ("androidx.compose.material3:material3:1.1.0")
-    implementation ("androidx.datastore:datastore-preferences:1.0.0")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     // Core Android e Lifecycle
     implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.compose.material3:material3:1.1.0")
-    implementation ("androidx.compose.material3:material3:1.1.0")
-    implementation ("androidx.compose.material:material-icons-core:1.4.3")
-    implementation ("androidx.compose.material:material-icons-extended:1.4.3")
-    implementation ("androidx.compose.foundation:foundation:1.4.3")
-    implementation ("androidx.compose.material3:material3:1.1.0")
-    implementation ("androidx.compose.material:material-icons-core:1.4.3")
-    implementation ("androidx.compose.material:material-icons-extended:1.4.3")
-    implementation ("androidx.compose.animation:animation:1.4.3")
-    implementation ("io.coil-kt:coil-compose:2.2.2")
 
-
-
-
-    // FirebaseUI
-
-
+    // Firebase
+    implementation(platform(libs.firebase.bom.v3231))
+    implementation(libs.firebase.ui.auth)
 
     // Testes
     testImplementation("junit:junit:4.13.2")
@@ -97,4 +82,3 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling:1.5.0")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.0")
 }
-
